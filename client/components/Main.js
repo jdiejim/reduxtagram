fb38198acb07ae93de9e736a1193a3d7ca539599
Juan Diego Jimenez <jdiejim@gmail.com>
2017-06-23 14:15:57 -0600

    add Main component

---
+++ client/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Main = () => {
+  return (
+    <main>
+      <h1>
+        <Link to="/">Reduxtagram</Link>
+      </h1>
+    </main>
+  )
+}
+
+export default Main;
